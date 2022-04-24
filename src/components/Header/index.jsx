595de47bc25b474803c45312e1b43391f9d3fda2
Ulugbek01 <xyz207820@gmail.com>
2022-04-24 12:26:58 +0500

    assets and components add

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './style';
+
+export const Header = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Header;
